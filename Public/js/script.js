65402d70ca9ff27a06c8dedced4ac731b1f956db
Ameetlohana <127467796+Ameetlohana@users.noreply.github.com>
2023-04-12 14:47:29 +1000

    Add database

--- Public/js/script.js
+++ Public/js/script.js
@@ -1,54 +1,51 @@
-const cardList = [
-    {
-        title: "Dog 2",
-        image: "Images/dog-png-22655.png",
-        link: "About Dog 2",
-        desciption: "Demo desciption about Dog 2"
-    },
-    {
-        title: "Dog 3",
-        image: "Images/Daco_4047293.png",
-        link: "About Dog 3",
-        desciption: "Demo desciption about Dog 3"
-    }
-]
-const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align"> <div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+ item.image +'"></img></div><div class="card-content"><span class="card-title activator grey-text text-darken-4">'+ item.title +'<i class="material-icons right">more_vert</i></span><p><a href="#">About this kiiten</a></p></div><div class="card-reveal"><span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span><p class="card-text">'+item.description+'</p></div></div></div>';
+        $('#card-section').append(itemToAppend);
+    });
+}
+
+const getProjects = () => {
+    $.get('/api/projects', (response) => {
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+    });
 }
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
 
-    console.log("Form Data Submitted: ", formData);
+    console.log('form data: ', formData);
+    addProjectToApp(formData);
 }
 
-const addCards = (items) => {
-    items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
+const addProjectToApp = (project) => {
+    $.ajax({
+        url: '/api/projects',
+        data: project,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
     });
 }
 
 $(document).ready(function(){
     $('.materialboxed').materialbox();
+    $('.modal').modal();
+
+    getProjects();
+
     $('#formSubmit').click(()=>{
         submitForm();
     })
-    addCards(cardList);
-    $('.modal').modal();
-  });
-
+});
 
 
 
