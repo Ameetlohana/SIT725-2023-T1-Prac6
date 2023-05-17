95029ea999e813854b5622929056e28a209b3fa5
Ameetlohana <127467796+Ameetlohana@users.noreply.github.com>
2023-05-17 11:01:49 +1000

    Task P7

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,22 +1,22 @@
 let model = require('../model/model');
 
-const insertCat = (req,res) => {
-    let cat = req.body;
-    model.insertCat(cat, (err, result) => {
-        if (err) {
-            res.json({statusCode: 400, message: err});
+const insertCat = (req, res) => {
+    let newProject = req.body;
+    model.insertCat(newProject, (error, result) => {
+        if (error) {
+            res.json({statusCode: 400, message: error});
         } else {
-            res.json({statusCode: 200, data: result, message: 'Cat Successfully Added'});
+            res.json({statusCode: 200, data: result, message: 'project successfully added'});
         }
     });
-}
+};
 
 const getAllCats = (req, res) => {
-    model.getAllCats((err, result) => {
-        if (err) {
-            res.json({statusCode: 400, message: err});
+    model.getAllCats((error, result) => {
+        if(error) {
+            res.json({statusCode: 400, message: error});
         } else {
-            res.json({statusCode: 200, data: result, message: 'Successful'});
+            res.json({statusCode: 200, data: result, message: 'Success'});
         }
     });
 }
--- dbConnection.js
+++ dbConnection.js
@@ -1,12 +1,13 @@
 const {MongoClient} = require('mongodb');
 const uri = 'mongodb+srv://ameethemnani9:dDeqmIYkpEtH1Nbx@cluster0.drtioeg.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true});
 
 client.connect(err => {
     if (!err) {
-        console.log('DB Connected');
+        console.log('Mongo DB connected');
     } else {
-        console.error(err);
+        console.log('Error: ', err);
+        process.exit(1);
     }
 });
 
